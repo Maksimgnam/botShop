60870bbd5110d7caf18147005a3ebc6fac63491f
UserName <m492615892gmail.com>
2023-04-18 12:57:06 +0300

    third

--- src/App.js
+++ src/App.js
@@ -1,20 +1,26 @@
 
 import { useEffect } from 'react';
+import { useTelegram } from './components/hooks/useTelegram';
 import './App.css';
 
 const tg = window.Telegram.WebApp;
 
 
 
+
 function App() {
+
+  const { onToggleButton, tg } = useTelegram()
 
   useEffect(() => {
     tg.ready()
-  })
+  }, [])
 
 
   return (
     <div className="App">
+
+      <button onClick={onToggleButton}>toggle </button>
 
 
 
